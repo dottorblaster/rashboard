c93b4c37f398acedaf42392237b49b6e4a1705da
Alessio Biancalana <dottorblaster@gmail.com>
2014-11-02 14:46:41 +0100

    widget drafting

--- widgets/example/view.js
+++ widgets/example/view.js
@@ -1,1 +1,14 @@
+// this is just a draft :F
+var tile = {
+	heading: "Draft of a tile",
+	description: "This is a string",
+	content: [
+		"data here"
+	],
+	// so we can expand the widget or reduce it but
+	// we have to know exactly where it breaks.
+	// This way we can avoid resizing it too much.
+	min-viewport: 4
+};
+
 console.log('loaded');
